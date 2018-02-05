99f5bd24397d4a0932b35813cd1c7dbca82e8bfe
GertS <schepens.gert@gmail.com>
2018-02-05 20:19:55 +0000

    Scripts de execution policy check ingevoerd, en basebox image en provider verandert.

--- DockerForWindows/scripts/Docker.ps1
+++ DockerForWindows/scripts/Docker.ps1
@@ -1,12 +1,21 @@
 # Aanpassen Windows PowerShell execution policy naar onbeperkt
-Set-ExecutionPolicy Unrestricted 
+$CurrentPolicy = get-ExecutionPolicy
+
+Write-Host "CURRENT POLICY IS $CurrentPolicy"
+
+if($CurrentPolicy -ne "Bypass")
+{
+    Write-Host "SETTING CURRENT POLICY TO Bypass"
+    Set-ExecutionPolicy Bypass
+}
+
 
 # Installeren Docker
 Install-Module DockerProvider -Force
 Install-Package Docker -ProviderName DockerProvider -Force
 
 # Start Docker
-Start-Service Docker
+#Start-Service Docker
 
 # Installeren posh-docker PowerShell module voor auto-completion
 Install-Module posh-docker
