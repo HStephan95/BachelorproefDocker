01f2ed02b7c8b189d668dbff1926824384138c27
DA-Gert Schepens <da-gert.schepens@zuto.com>
2018-02-05 22:04:36 +0000

    Basebox Vagrant file aangepast voor Docker installatie.

--- DockerForWindows/scripts/Docker.ps1
+++ DockerForWindows/scripts/Docker.ps1
@@ -1,3 +1,5 @@
+
+<#
 # Aanpassen Windows PowerShell execution policy naar onbeperkt
 $CurrentPolicy = get-ExecutionPolicy
 
@@ -9,13 +11,27 @@ if($CurrentPolicy -ne "Bypass")
     Set-ExecutionPolicy Bypass
 }
 
-
+Write-Host "#GOING to INSTALL DOCKER#"
 # Installeren Docker
 Install-Module DockerProvider -Force
 Install-Package Docker -ProviderName DockerProvider -Force
-
+Write-Host "#GOING to START DOCKER#"
 # Start Docker
-#Start-Service Docker
+$ServiceToStart = Get-Service -Name Docker
+
+$ServiceToStart
+
+Start-Service $ServiceToStart
+
+Start-Sleep -Seconds 5
+
+Get-Service -Name Docker
+
+Start-Sleep -Seconds 5
+
+Write-Host "#SHIT WENT DOWN HERE#"
+
+#>
 
 # Installeren posh-docker PowerShell module voor auto-completion
 Install-Module posh-docker
