3a843a688c9bcb0b0513d53071868e0427488507
DA-Gert Schepens <da-gert.schepens@zuto.com>
2018-03-23 21:26:24 +0000

    App deploy Success! - SQL Connect FAIL

--- DockerForWindows/provisioning/applicaties.ps1
+++ DockerForWindows/provisioning/applicaties.ps1
@@ -1,29 +1,26 @@
 
-cd ../../Users/vagrant
+cd ../../Users/vagrant/VoorbeeldProject/DienstenCheques
 
-new-item ".\app\Dockerfile" -Force 
+new-item "Dockerfile" -Force 
+New-Item "app" -ItemType Directory -Force
 
 $DockerCommands = 'FROM microsoft/dotnet:2.0.5-sdk-2.1.4'
- $DockerCommands | Add-Content ".\app\Dockerfile"
-$DockerCommands = 'WORKDIR \app'
-$DockerCommands | Add-Content ".\app\Dockerfile"
-$DockerCommands = 'COPY voorbeeldproject .\app'
- $DockerCommands | Add-Content ".\app\Dockerfile"
+ $DockerCommands | Add-Content "Dockerfile"
+$DockerCommands = 'WORKDIR /app'
+$DockerCommands | Add-Content "Dockerfile"
+$DockerCommands = 'COPY . /app'
+ $DockerCommands | Add-Content "Dockerfile"
 $DockerCommands = 'RUN dotnet restore'
- $DockerCommands | Add-Content ".\app\Dockerfile"
+ $DockerCommands | Add-Content "Dockerfile"
 $DockerCommands = 'RUN dotnet build'
- $DockerCommands | Add-Content ".\app\Dockerfile"
+ $DockerCommands | Add-Content "Dockerfile"
 $DockerCommands = 'EXPOSE 5000/tcp'
- $DockerCommands | Add-Content ".\app\Dockerfile"
+ $DockerCommands | Add-Content "Dockerfile"
 $DockerCommands = 'ENV ASPNETCORE_URLS http://*:5000'
- $DockerCommands | Add-Content ".\app\Dockerfile"
+ $DockerCommands | Add-Content "Dockerfile"
 $DockerCommands = 'ENTRYPOINT ["dotnet", "run"] ' 
- $DockerCommands | Add-Content ".\app\Dockerfile"
-
-cd ../../Users/vagrant/app
+ $DockerCommands | Add-Content "Dockerfile"
 
 docker build -t voorbeeldproject:myapp .
 
 docker run -d -p 8080:5000 -t voorbeeldproject:myapp
-
-#>
